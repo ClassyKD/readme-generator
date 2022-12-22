45a985867c207af825625242b2a39cede9e169ff
kdmoreno14-LNflrw <kdmoreno14@gmail.com>
2022-12-22 10:44:36 -0800

    array of questions created

--- index.js
+++ index.js
@@ -1,13 +1,79 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    //Title
+    {
+        type: 'input',
+        message: 'What is the title of your program?',
+        name: 'title'
+    },
+    //description
+    {
+        type: 'input',
+        message: 'Type a description of your program',
+        name: 'description'
+    },
+    //Installation
+    {
+        type: 'input',
+        message: 'How do you install the program',
+        name: 'installation'
+    },
+    //Usage
+    {
+        type: 'input',
+        message: 'What is the usage of the program?',
+        name: 'Usage'
+    },
+    //License
+    {
+        type: 'checkbox',
+        message: 'pick all Licenses that apply.',
+        name: 'License',
+        choices: ['Apache 2.0', 'Boost Software', 'BSD 3-Clause', 'CC0', 'GNU GPL v3'],
+    },
+    //Contribution
+    {
+        type: 'input',
+        message: 'Enter contributions to this project.',
+        name: 'contribution'
+    },
+    //Tests
+    {
+        type: 'input',
+        message: 'Type a description of your program',
+        name: 'tests'
+    },
+    //Git Username
+    {
+        type: 'input',
+        message: 'Enter Github Username.',
+        name: 'gitUser'
+    },
+    //Email
+    {
+        type: 'input',
+        message: 'Enter your email.',
+        name: 'email'
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//creates README file with name and content
+function writeToFile(fileName, data) {
+    fs.appendFile('README.txt', finishedReadMe, (err) =>
+    err ? console.error(err) : console.log('README created!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+
+function init() {
+
+}
 
 // Function call to initialize app
 init();
